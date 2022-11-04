7c187acdd3c6f6b892eb915243449f34b42ec686
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-04 17:25:29 +0900

    新規登録機能を実装
    ※バリデーションは設定したものの、再度確認する予定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ヶ]+\z/
+
+  validates :nickname, presence: true
+  validates :password,
+            format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers', allow_blank: true }
+  with_options presence: true,
+               format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters',
+                         allow_blank: true } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true,
+               format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters',
+                         allow_blank: true } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
+  validates :birth_date, presence: true
 end
