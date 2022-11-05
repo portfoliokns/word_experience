3ec776f29bb89b564a36f938d412e69c0b3ef3b2
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-05 22:12:08 +0900

    一部機能修正
    更新ボタンを押すと、削除機能が働いてしまい、ユーザー情報が消えてしまう問題を修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it '' do
+      end
+    end
+
+    context "新規登録できない場合" do
+      it '' do
+      end
+      it '' do
+      end
+    end
+  end
 end
