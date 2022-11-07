15cfd1197f236959229d8a815e133fc8a437f1c6
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-07 14:13:23 +0900

    ActiveHashの一部記載漏れを追加

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,8 @@
 class Word < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :main_category
+  belongs_to :service_category
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :main_category_id, numericality: { other_than: 1, message: "can't be blank" }
