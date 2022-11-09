bbc84b77c60d3d2eec061eef5f40122fff045d06
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-09 21:11:17 +0900

    単体テストコード実装
    以下の警告文を回避するため、case_sensitibe: trueをバリデーションに追加。
    DEPRECATION WARNING: Uniqueness validator will no longer enforce case sensitive comparison in Rails 6.1. To continue case sensitive comparison on the :name attribute in Word model, pass `case_sensitive: true` option explicitly to the uniqueness validator.

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,8 +5,8 @@ class Word < ApplicationRecord
   belongs_to :service_category
 
   validates :name, presence: true,
-            uniqueness: { scope: :user },
-            length: { minimum: 4, maximum: 30 }
+            uniqueness: { scope: :user, case_sensitive: true },
+            length: { minimum: 4, maximum: 30 , allow_blank: true}
   validates :main_category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :service_category_id, numericality: { other_than: 1, message: "can't be blank" }
 end
