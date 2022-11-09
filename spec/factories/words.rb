6ad4dec083858c66400a6421197ea8bfc38825d2
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-09 16:41:05 +0900

    FactoryBot準備

--- spec/factories/words.rb
+++ spec/factories/words.rb
@@ -1,5 +1,12 @@
+random = Random.new()
 FactoryBot.define do
   factory :word do
-    
+    name_num = random.rand(3..30)
+    main_category_max_id = 5
+    service_category_max_id = 5
+    name                  { Faker::Lorem.characters(number: name_num ) }
+    main_category_id      { Faker::Number.between(from: 2, to: main_category_max_id) }
+    service_category_id   { Faker::Number.between(from: 2, to: service_category_max_id) }
+    association :user
   end
 end
