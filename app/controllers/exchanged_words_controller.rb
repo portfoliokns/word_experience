b2172c90c86e0243ba478a038822ff83578364e9
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-12 01:01:58 +0900

    ビューの仮設定終了

--- app/controllers/exchanged_words_controller.rb
+++ app/controllers/exchanged_words_controller.rb
@@ -1,9 +1,15 @@
 class ExchangedWordsController < ApplicationController
   def index
-
+    @words = Word.where(user_id: current_user.id).order('updated_at DESC')
+    @main_category = MainCategory.all
+    @service_category = ServiceCategory.all
   end
 
   def new
+
+  end
+
+  def create
 
   end
 end
