d9797de25c5d5c8ec761d6639fcb4ba48a26ec0b
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-13 16:53:44 +0900

    Revert "ワードの生成機能を実装"
    This reverts commit 4ac66b0d571dc14130a40ccb40f0290ebe3c86e4.

--- app/controllers/exchanged_words_controller.rb
+++ app/controllers/exchanged_words_controller.rb
@@ -10,34 +10,6 @@ class ExchangedWordsController < ApplicationController
   end
 
   def create
-    exchanged_word_params
-    words = Word.where.not(user_id: current_user.id).order("RAND()").limit(2)
-    if save_exchanged_word_or_exchanged_words(words)
-      redirect_to user_exchanged_words_path(current_user.id)
-    else
-      render :new
-    end
-  end
 
-  private
-  def exchanged_word_params
-    aa = params.permit(:num)
-  end
-
-  def save_exchanged_word_or_exchanged_words(words)
-    is_success = true
-    ActiveRecord::Base.transaction do
-      words.each do |word|
-        exchanged_word = ExchangedWord.new()
-        exchanged_word.user_id = word.user_id
-        exchanged_word.word_id = word.id
-        is_success = false unless exchanged_word.save
-      end
-      raise ActiveRecord::RollBack unless is_success
-    end
-  rescue
-    p 'transaction error'
-  ensure
-    return is_success
   end
 end
