4ac66b0d571dc14130a40ccb40f0290ebe3c86e4
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-13 16:53:22 +0900

    ワードの生成機能を実装
    自分以外のユーザーから、ワードを交換する機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "toppage#index"
   resources :users, only: [:index] do
     resources :words, only: [:index, :new, :create, :edit, :update, :destroy]
-    resources :exchanged_words, only: [:index, :new]
+    resources :exchanged_words, only: [:index, :new, :create]
   end
 end
