975265343bb5636400f819460d6ce38d4ace70e5
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-16 14:15:43 +0900

    ワードポイント：単体テスト洗い出し

--- spec/factories/word_points.rb
+++ spec/factories/word_points.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :word_point do
-    
+    association :user
+    point            { Faker::Number.between(from: 0, to: 999_999_999) }
   end
 end
--- spec/models/word_point_spec.rb
+++ spec/models/word_point_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe WordPoint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @word_point = FactoryBot.build(:word_point)
+    sleep 0.01
+  end
+
+  describe 'ワードポイントの登録' do
+    context '情報が登録できる場合' do
+      it 'ワードポイント(整数)が正しく登録できる' do
+        # expect(@word_point).to be_valid
+      end
+    end
+
+    context "登録できない場合" do
+      it 'ワードポイントが空である' do
+        # @word_point.name = ''
+        # @word_point.valid?
+        # expect(@word_point.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'ワードポイントが整数でない' do
+        
+      end
+
+      it 'ワードポイントが0より小さい数字である(-1以下の整数)' do
+        
+      end
+
+      it 'ワードポイントが999,999,999より大きい数字である(1,000,000,000以上の整数)' do
+        
+      end
+
+      it 'ユーザーIDが空である' do
+        
+      end
+    end
+  end
 end
