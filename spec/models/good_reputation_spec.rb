7a179b7cc506090777b22f0a056eac33adbe7ceb
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-22 20:31:09 +0900

    単体テストコード追加

--- spec/models/good_reputation_spec.rb
+++ spec/models/good_reputation_spec.rb
@@ -9,17 +9,16 @@ RSpec.describe GoodReputation, type: :model do
   describe '評価の登録' do
     context '評価が登録できる場合' do
       it 'star_flagがTrueまたはFalseで登録できる' do
-        # expect(@good_reputation).to be_valid
+        expect(@good_reputation).to be_valid
       end
     end
 
     context "登録できない場合" do
       it 'star_flagがTrue、Falseでない' do
-        # @good_reputation = ''
-        # @good_reputation.valid?
-        # expect(@good_reputation.errors.full_messages).to include("Point can't be blank")
+        @good_reputation.star_flag = ''
+        @good_reputation.valid?
+        expect(@good_reputation.errors.full_messages).to include("Star flag is not included in the list")
       end
-
     end
   end
 
