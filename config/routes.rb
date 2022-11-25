0de3b630afa16f5314b8a566a37935aa69d00510
konishi333 <jobhuntingkonishi3@gmail.com>
2022-11-25 23:38:04 +0900

    マニュアルコントローラーを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "toppage#index"
+  resources :manuals, only: [:index]
   resources :users, only: [:index] do
     resources :words, only: [:index, :new, :create, :edit, :update, :destroy]
     resources :exchanged_words, only: [:index, :new, :create, :show] do
