355f99fac22690ceacdfa5a6747e164247433133
konishi333 <jobhuntingkonishi3@gmail.com>
2022-12-06 09:57:04 +0900

    プロフィール情報の計算処理を追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
   def index
-    
+    @good_reputation_total = 67
+    @bad_reputation_total = 32
+    result = (@good_reputation_total.quo(@good_reputation_total + @bad_reputation_total)).to_f * 100
+    @rate = result.round(1)
   end
 end
