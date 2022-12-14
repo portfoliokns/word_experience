298f4429b4fc0bd8d9fcd91be6f7f96bf12cd0d9
konishi333 <jobhuntingkonishi3@gmail.com>
2022-12-14 23:26:43 +0900

    ブートストラップに関わるコードを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("shared/sidebar")
-import "bootstrap"
-import "../stylesheets/application"
 require("copy_clipboard.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
