7925b742a1f8a827a18a6dba8dfbcdf5a557259f
konishi333 <jobhuntingkonishi3@gmail.com>
2022-12-27 01:03:50 +0900

    リンク先がないサービスはそのページに止まるよう処理を実装
    実現のために以下のように対応
    ・サービスカテゴリに、リンク先があるかどうかを判断するためのフラグ(link)を用意
    ・フラグを元に、HTML側で条件分岐させ、falseの場合に新規タブで開かれないように処理を実装

--- app/models/service_category.rb
+++ app/models/service_category.rb
@@ -1,33 +1,33 @@
 class ServiceCategory < ActiveHash::Base
   self.data = [
     { id: 1, name: '--サービスを選択必須--' },
-    { id: 101, name: 'Googole', link: 'https://www.google.com/' },
-    { id: 102, name: 'Googole Maps', link: 'https://www.google.co.jp/maps/' },
-    { id: 103, name: 'Googole トレンド', link: 'https://trends.google.co.jp/' },
-    { id: 104, name: 'Youtube', link: 'https://www.youtube.com/' },
+    { id: 101, name: 'Googole', url: 'https://www.google.com/', link: true },
+    { id: 102, name: 'Googole Maps', url: 'https://www.google.co.jp/maps/', link: true },
+    { id: 103, name: 'Googole トレンド', url: 'https://trends.google.co.jp/', link: true },
+    { id: 104, name: 'Youtube', url: 'https://www.youtube.com/', link: true },
 
-    { id: 201, name: 'Amazon', link: 'https://www.amazon.co.jp/' },
-    { id: 202, name: 'prime video', link: 'https://www.amazon.co.jp/Prime-Video/b?node=3535604051' },
-    { id: 203, name: 'Kindle', link: 'https://www.amazon.co.jp/Kindle-%E3%82%AD%E3%83%B3%E3%83%89%E3%83%AB-%E9%9B%BB%E5%AD%90%E6%9B%B8%E7%B1%8D/b?ie=UTF8&node=2250738051' },
-    { id: 204, name: 'AWS', link: 'https://aws.amazon.com/' },
+    { id: 201, name: 'Amazon', url: 'https://www.amazon.co.jp/', link: true },
+    { id: 202, name: 'prime video', url: 'https://www.amazon.co.jp/Prime-Video/b?node=3535604051', link: true },
+    { id: 203, name: 'Kindle', url: 'https://www.amazon.co.jp/Kindle-%E3%82%AD%E3%83%B3%E3%83%89%E3%83%AB-%E9%9B%BB%E5%AD%90%E6%9B%B8%E7%B1%8D/b?ie=UTF8&node=2250738051', link: true },
+    { id: 204, name: 'AWS', url: 'https://aws.amazon.com/', link: true },
 
-    { id: 301, name: 'Twitter', link: 'https://twitter.com/' },
-    { id: 302, name: 'Twitter(マーケティング)', link: 'https://marketing.twitter.com/' },
+    { id: 301, name: 'Twitter', url: 'https://twitter.com/', link: true },
+    { id: 302, name: 'Twitter(マーケティング)', url: 'https://marketing.twitter.com/', link: true },
 
 
-    { id: 401, name: 'LINE' },
-    { id: 402, name: 'LINEスタンプ' },
+    { id: 401, name: 'LINE', url: '#', link: false },
+    { id: 402, name: 'LINEスタンプ', url: '#', link: false },
 
-    { id: 501, name: 'Facebook', link: 'https://www.facebook.com/' },
+    { id: 501, name: 'Facebook', url: 'https://www.facebook.com/', link: true },
 
-    { id: 601, name: 'Netflix', link: 'https://www.netflix.com/' },
+    { id: 601, name: 'Netflix', url: 'https://www.netflix.com/', link: true },
 
-    { id: 701, name: 'メルカリ', link: 'https://jp.mercari.com/' },
+    { id: 701, name: 'メルカリ', url: 'https://jp.mercari.com/', link: true },
 
-    { id: 801, name: 'Yahoo!', link: 'https://www.yahoo.co.jp/' },
-    { id: 802, name: 'Yahoo!ショッピング', link: 'https://shopping.yahoo.co.jp/?sc_e=ytmh' },
+    { id: 801, name: 'Yahoo!', url: 'https://www.yahoo.co.jp/', link: true },
+    { id: 802, name: 'Yahoo!ショッピング', url: 'https://shopping.yahoo.co.jp/?sc_e=ytmh', link: true },
 
-    { id: 99999, name: 'その他' }
+    { id: 99999, name: 'その他', url: '#', link: false }
   ]
 
   include ActiveHash::Associations
