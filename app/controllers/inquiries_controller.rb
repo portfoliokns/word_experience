2a5a25473d90db6f998ce4e3041d8b27127101fc
konishi333 <jobhuntingkonishi3@gmail.com>
2023-02-15 11:48:07 +0900

    アクセスの制限機能を実装
    ログインしていないユーザーはアクセスできないように機能を実装。また一部コードをリファクタリング

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,28 +1,32 @@
 class InquiriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_inquiry, only: [:confirm, :create, :back]
+
   def new
     @Inquiry = Inquiry.new
   end
 
   def confirm
-    @Inquiry = Inquiry.new(inquiry_params)
     if @Inquiry.invalid?
       render :new
     end
   end
 
   def create
-    @Inquiry = Inquiry.new(inquiry_params)
     # NoticeMailer.send_admin(@Inquiry).deliver_now
     # NoticeMailer.send_user(@Inquiry).deliver_now
     redirect_to send_inquiry_path
   end
 
   def back
-    @Inquiry = Inquiry.new(inquiry_params)
     render :new
   end
 
   private
+  
+  def set_inquiry
+    @Inquiry = Inquiry.new(inquiry_params)
+  end
 
   def inquiry_params
     params.require(:inquiry).permit(:name,:email,:detail)
