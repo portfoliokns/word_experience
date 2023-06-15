f2743455bd7c8b69a795dee60388197a1c728118
konishi333 <jobhuntingkonishi3@gmail.com>
2023-06-15 23:29:15 +0900

    POC対応
    検討している要件の通りに、実現できるかを確認。その結果、要件通りに実現できそうとなった

--- app/models/word_collection.rb
+++ app/models/word_collection.rb
@@ -17,6 +17,45 @@ class WordCollection
     end
   end
 
+  def input_valid?(save_params, error_message)
+
+    word_names = save_params.map { |params| params[:name] }
+
+    if word_names.length != word_names.uniq.length
+      error_message.replace("同じ名前の単語が2つ以上存在します")
+      return false
+    end
+
+    save_params.each do |params|
+      if params[:name].empty?
+        error_message.replace("ワードを入力してください")
+        return false
+      end
+  
+      if Word.exists?(name: params[:name])
+        error_message.replace("同じ名前の単語が既に存在します")
+        return false
+      end
+
+      word = Word.new(params)
+      if word.invalid?
+        error_message.replace("ワードは4文字以上、30文字以下で入力してください")
+        return false
+      end
+  
+      if params[:main_category_id] == "1"
+        error_message.replace("カテゴリを選択してください")
+        return false
+      end
+  
+      if params[:service_category_id] == "1"
+        error_message.replace("サービスを選択してください")
+        return false
+      end
+    end
+    return true
+  end
+
   def save_data(params)
     is_success = true
     ActiveRecord::Base.transaction do
