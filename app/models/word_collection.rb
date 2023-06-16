8db628c8bba2371c30cb5d5e23c001c2ef7186df
konishi333 <jobhuntingkonishi3@gmail.com>
2023-06-16 20:26:47 +0900

    入力チェック機能を実装

--- app/models/word_collection.rb
+++ app/models/word_collection.rb
@@ -18,41 +18,51 @@ class WordCollection
   end
 
   def input_valid?(save_params, error_message)
-
-    word_names = save_params.map { |params| params[:name] }
-
-    if word_names.length != word_names.uniq.length
-      error_message.replace("同じ名前の単語が2つ以上存在します")
-      return false
-    end
+    row_count = 1
+    names_list = []
 
     save_params.each do |params|
       if params[:name].empty?
-        error_message.replace("ワードを入力してください")
-        return false
-      end
-  
-      if Word.exists?(name: params[:name])
-        error_message.replace("同じ名前の単語が既に存在します")
+        message = "#{row_count}行目：ワードを入力してください。"
+        error_message.replace(message)
         return false
       end
 
-      word = Word.new(params)
-      if word.invalid?
-        error_message.replace("ワードは4文字以上、30文字以下で入力してください")
+      if names_list.include?(params[:name])
+        message = "#{row_count}行目の'#{params[:name]}'：同じワードを登録することはできません。"
+        error_message.replace(message)
+        return false
+      else
+        names_list << params[:name]
+      end
+
+      if Word.exists?(name: params[:name])
+        message = "#{row_count}行目：同じ名前の単語が既に登録されています。"
+        error_message.replace(message)
         return false
       end
-  
+
+      if params[:name].length <= 3
+        message = "#{row_count}行目：ワードは4文字以上、30文字以下で入力してください。"
+        error_message.replace(message)
+        return false
+      end
+
       if params[:main_category_id] == "1"
-        error_message.replace("カテゴリを選択してください")
+        message = "#{row_count}行目：カテゴリを選択してください。"
+        error_message.replace(message)
         return false
       end
   
       if params[:service_category_id] == "1"
-        error_message.replace("サービスを選択してください")
+        message = "#{row_count}行目：サービスを選択してください。"
+        error_message.replace(message)
         return false
       end
+
+      row_count += 1
     end
+
     return true
   end
 
