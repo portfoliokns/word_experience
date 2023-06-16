1b00789d532ec05760627fe650090d1ea38616b9
konishi333 <jobhuntingkonishi3@gmail.com>
2023-06-16 23:06:22 +0900

    エラーメッセージを整理し、リファクタリング

--- app/models/word_collection.rb
+++ app/models/word_collection.rb
@@ -6,6 +6,7 @@ class WordCollection
   include ActiveModel::Validations
   WORD_NUM = 4
   attr_accessor :collection
+  include ErrorMessageFlash
 
   def new_set_data
     self.collection = WORD_NUM.times.map { Word.new }
@@ -25,13 +26,13 @@ class WordCollection
 
     save_params.each do |params|
       if params[:name].empty?
-        message = "#{row_count}項目：ワードが入力されていません。入力してください。"
+        message = get_word_empty_message(row_count)
         error_message.replace(message)
         return false
       end
 
       if names_list.include?(params[:name])
-        message = "#{row_count}項目：同じワード '#{params[:name]}' を登録しようとしています。他のワードを入力してください。"
+        message = get_word_duplicate_message(row_count, params[:name])
         error_message.replace(message)
         return false
       else
@@ -39,25 +40,25 @@ class WordCollection
       end
 
       if Word.exists?(name: params[:name])
-        message = "#{row_count}項目：同じワード '#{params[:name]}' が既に登録されています。別のワードを入力してください。"
+        message = get_word_registered_message(row_count,params[:name])
         error_message.replace(message)
         return false
       end
 
       if params[:name].length < minimum_length
-        message = "#{row_count}項目：ワードは#{minimum_length}文字以上、30文字以下で入力してください。"
+        message = get_word_length_message(row_count, minimum_length)
         error_message.replace(message)
         return false
       end
 
       if params[:main_category_id] == MainCategory::CategorySelectId.to_s
-        message = "#{row_count}項目：カテゴリを選択してください。"
+        message = get_main_category_message(row_count)
         error_message.replace(message)
         return false
       end
   
       if params[:service_category_id] == ServiceCategory::CategorySelectId.to_s
-        message = "#{row_count}項目：サービスを選択してください。"
+        message = get_service_category_message(row_count)
         error_message.replace(message)
         return false
       end
